daa73857b282726e0a70e4f6fc4a4958d56f7693
Sticktron <sticktron@hotmail.com>
2017-05-13 10:50:33 -0400

    New cleaning script

--- clean.sh
+++ clean.sh
@@ -1,19 +1,46 @@
 #!/bin/bash
-make clean
 
-echo "removing .DS_Store files"
+echo
+echo ">> deleting '.DS_Store' files..."
 find . -name '.DS_Store' -delete
 
-echo "removing /obj"
+echo
+echo ">> cleaning..."
+make clean
+
+echo
+echo ">> deleting 'obj' folders..."
 rm -rf obj
 rm -rf Prefs/obj
 
-echo "removing /packages"
-rm -rf packages
-rm -rf Prefs/packages
+echo
+read -p "Remove packages? (y/n) " answer
+while true
+do
+	case $answer in
+		[yY]* ) echo ">> deleting 'packages' folders..."
+				rm -rf packages
+				rm -rf Prefs/packages				
+				break;;
+		* )		echo ">> skipping 'packages' folders"
+				break;;
+	esac
+done
 
-echo "removing /.theos"
-rm -rf .theos
-rm -rf Prefs/.theos
+echo
+read -p "Remove .theos? " answer
+while true
+do
+	case $answer in
+		[yY]* ) echo ">> deleting '.theos' folders..."
+				rm -rf .theos
+				rm -rf Prefs/.theos
+				break;;
+		* )		echo ">> skipping '.theos' folders"
+				break;;
+	esac
+done
 
-echo "done."
+echo
+echo "Done."
+echo
